bbaacafab17d7c957379fe595d458973816ca568
moritzhckr <moritz.haecker@online.de>
2021-07-28 18:00:45 +0200

    Removed Debug Drawline

--- Runtime/Scripts/Vivid_ThirdPersonCharacter.cs
+++ Runtime/Scripts/Vivid_ThirdPersonCharacter.cs
@@ -205,7 +205,7 @@ public class Vivid_ThirdPersonCharacter : MonoBehaviour
 			RaycastHit hitInfo;
 #if UNITY_EDITOR
 			// helper to visualise the ground check ray in the scene view
-			Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
+			//Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
 #endif
 			// 0.1f is a small offset to start the ray from inside the character
 			// it is also good to note that the transform position in the sample assets is at the base of the character
